c521429b31b96da88001896c37c519fa8d7319a6
mklmyers <54815794+mklmyers@users.noreply.github.com>
2019-09-02 14:28:40 -0400

    Revisions
    I don't know how to do much on this website, so this is my best guess. I didn't understand the getReverse() method and it kept endlessly trying to run for me so I rewrote it and got it to work. I also got the getReverseComplement() method working. It calls both other methods in one line and returns the reverse complement. It's all that I call in main() to make it work. I think all that's left is the rest of the JavaDocs, further testing, and cleaning it up.

--- StringDNA.java
+++ StringDNA.java
@@ -1,11 +1,10 @@
-import java.util.Arrays;
 /**
  * Returns the reverse, complement, and reverse complement of half of a DNA string.
  * <br>getReverse will return reverse the DNA provided.
  * <br>getComplement will return the complement of the DNA provided.
  * <br>getReverseComplement will return the reverse complement of the DNA provided.
  *
- * @author Makarie Myers
+ * @author MaKarie Myers
  * @author Valentina Young-Wood
  * @version 9/2/2019
  * @param original the original half of a string of DNA
@@ -13,39 +12,35 @@ import java.util.Arrays;
  */
 public class StringDNA
 {
-    String rev;
-    String comp;
-    String revcomp;
-    
     public static void main(){
         StringDNA dna = new StringDNA();
-        String test = "ATG";
-        dna.getReverse(test);
-        System.out.println("The reverse of "+test+" is "+dna.rev);
-        dna.getComplement(test);
-        System.out.println("The complement of "+test+" is "+dna.comp);
-        dna.getReverseComplement(test);
-        System.out.println("The reverse complement of "+test+" is "+dna.revcomp);
+        System.out.println(dna.getReverseComplement("ATG"));
+        //String test = "ATG";
+        //System.out.println("The reverse of "+test+" is "+dna.getReverse(test));
+        //System.out.println("The complement of "+test+" is "+dna.getComplement(test));
+        //System.out.println("The reverse complement of "+test+" is "+dna.getReverseComplement(test));
     }
     
     public String getReverse(String original){
-        String[] origin = original.split("");
-        String[] reverse = new String[origin.length];
-        for (int i=origin.length; i>0; i--){
-            for(int x= 0; i<reverse.length; i++){
-                reverse[x] = origin[i];
-            }
+        int index = (original.length());
+        char[] origin = original.toCharArray();
+        char[] reverse = new char[index];
+        int j = 0;
+        for (int i=index-1; i>=0; i--)
+        {
+            reverse[j] = origin[i];
+            j++;
         }
-        String rev= Arrays.toString(reverse);
-        //Needs to return GTA.
+        String rev= new String(reverse);
         return rev;
+        //Needs to return GTA.
     }
     
-    public String getComplement(String original){
-        int index = (original.length()); //saves the length of the given string to an int
-        char [] dna = original.toCharArray(); //converts the given string to a character array
+    public String getComplement(String input){
+        int index = (input.length()); //saves the length of the given string to an int
+        char [] dna = input.toCharArray(); //converts the given string to a character array
         char [] comp = new char[index]; //creates a character array with a length equal to the length of the original string
-        for (int i = 0; i < index; i++)
+        for (int i = 0; i < index; i++) 
         {
             if (dna[i] == 'A') //if the given string had an A, put a T in the complement
             {
@@ -68,12 +63,14 @@ public class StringDNA
         }
         String complement = new String(comp); //converts the complement char array into a string
         return complement; //returns the complement as a String
+        //Needs to return TAC.
     }
     
     public String getReverseComplement(String original){
-        getComplement(original);
-        getReverse(comp);
+        String revComp = getReverse(getComplement(original));
+        //getComplement(original);
+        //getReverse(comp);
         //Needs to return CAT.
-        return revcomp;
+        return revComp;
     }
 }
